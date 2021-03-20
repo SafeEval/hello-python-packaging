25a9bf14191c6564fd14ae79094a64cbd282a9c7
Jack Sullivan <jack@divergent.codes>
2021-03-20 08:22:32 -0700

    test(coverage): Added function without coverage

--- hello_pp/cli.py
+++ hello_pp/cli.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
+"""
+Top level CLI module.
+"""
 
 from .__version__ import __version__
 
 
 def main():
+    """ CLI entrypoint.
+    """
     print(f'Hello Python Project! ({__version__})')
+
+
+def untested():
+    """ Untested function for code coverage.
+    """
+    print('This function is untested. Will show up in code coverage.')
