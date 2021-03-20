0243977b522c80280579f86dcd4d7b85fe072851
Jack Sullivan <jack@divergent.codes>
2021-03-20 11:11:42 -0700

    Commented out some of the untested code

--- hello_pp/untested.py
+++ hello_pp/untested.py
@@ -7,25 +7,25 @@ def this_has_no_test():
     print('Untested')
 
 
-def this_has_no_test2():
-    print('Untested')
+# def this_has_no_test2():
+#     print('Untested')
 
 
-def this_has_no_test3():
-    print('Untested')
+# def this_has_no_test3():
+#     print('Untested')
 
 
-def this_has_no_test4():
-    print('Untested')
+# def this_has_no_test4():
+#     print('Untested')
 
 
-def this_has_no_test5():
-    print('Untested')
+# def this_has_no_test5():
+#     print('Untested')
 
 
-def this_has_no_test6():
-    print('Untested')
+# def this_has_no_test6():
+#     print('Untested')
 
 
-def this_has_no_test():
-    print('Untested')
+# def this_has_no_test():
+#     print('Untested')
