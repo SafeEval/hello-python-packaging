3c4ffec2318a0c346224c5e2b411665d0e2af37b
Jack Sullivan <jack@divergent.codes>
2021-03-20 10:35:58 -0700

    Add code without coverage

--- hello_pp/untested.py
+++ hello_pp/untested.py
@@ -5,3 +5,27 @@ This Python module has no unit tests, which will show up during code coverage.
 
 def this_has_no_test():
     print('Untested')
+
+
+def this_has_no_test2():
+    print('Untested')
+
+
+def this_has_no_test3():
+    print('Untested')
+
+
+def this_has_no_test4():
+    print('Untested')
+
+
+def this_has_no_test5():
+    print('Untested')
+
+
+def this_has_no_test6():
+    print('Untested')
+
+
+def this_has_no_test():
+    print('Untested')
