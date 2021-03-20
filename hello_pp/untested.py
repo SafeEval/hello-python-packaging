3c998d54be250d8788e4cea6a250db84de9d8210
Jack Sullivan <jack@divergent.codes>
2021-03-20 11:31:38 -0700

    Adding just a little untested code

--- hello_pp/untested.py
+++ hello_pp/untested.py
@@ -11,8 +11,8 @@ def this_has_no_test2():
     print('Untested')
 
 
-# def this_has_no_test3():
-#     print('Untested')
+def this_has_no_test3():
+    print('Untested')
 
 
 # def this_has_no_test4():
