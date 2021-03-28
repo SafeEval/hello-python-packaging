74ecf65aab206130c2ac62d60e7602c1ba7c88b9
Jack Sullivan <jack@divergent.codes>
2021-03-27 18:00:37 -0700

    style: Formatted docstrings

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,8 +6,7 @@ from hello_pp.__version__ import __version__
 
 
 def test_cli_main_prints_version(capsys):
-    """ Test CLI version display
-    """
+    """Test CLI version display"""
     cli.main()
     captured_stdout = capsys.readouterr()
     assert __version__ in captured_stdout.out
