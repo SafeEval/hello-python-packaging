a5fe260e66b05838ded63746528d1aedd466215f
sully90h <5807759+sully90h@users.noreply.github.com>
2021-04-06 08:55:03 -0700

    Create forked_code.py

---
+++ hello_pp/forked_code.py
@@ -0,0 +1,37 @@
+"""
+This Python module has no unit tests, which will show up during code coverage.
+"""
+
+def trigger_bandit():
+    """ Semgrep bandit trigger """
+    import hashlib
+    hashlib.sha1(1)
+
+def this_has_no_test():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test2():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test3():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test4():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test5():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test6():
+    """ Untested code """
+    print("Untested")
