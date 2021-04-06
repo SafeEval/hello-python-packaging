456619fa8b2ee79f27fb84e96380d32eea3da2b5
Jack Sullivan <jack@divergent.codes>
2021-04-06 07:20:16 -0700

    Removed untested code

--- hello_pp/untested.py
+++ hello_pp/untested.py
@@ -3,9 +3,9 @@ This Python module has no unit tests, which will show up during code coverage.
 """
 
 
-def this_has_no_test():
-    """ Untested code """
-    print("Untested")
+# def this_has_no_test():
+#     """ Untested code """
+#     print("Untested")
 
 
 # def this_has_no_test2():
