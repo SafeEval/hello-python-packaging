f8aa504c82e17d7b1ec03260ab725d5239db79fc
sully90h <5807759+sully90h@users.noreply.github.com>
2021-04-06 08:16:18 -0700

    Create fork_fails.py

---
+++ hello_pp/fork_fails.py
@@ -0,0 +1,33 @@
+"""
+This Python module has no unit tests, which will show up during code coverage.
+"""
+
+
+def this_has_no_test():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test2():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test3():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test4():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test5():
+    """ Untested code """
+    print("Untested")
+
+
+def this_has_no_test6():
+    """ Untested code """
+    print("Untested")
